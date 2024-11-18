45cecdbabf381e2db8f3d77bd927d3eb5a7a182f
Богдан Колганов <kolganov_25@mail.ru>
2024-11-18 18:56:01 +0700

    -fix

---
+++ Sem3_Coursework/Products/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3_Coursework.Products
+{
+    public enum Units
+    {
+        kg, pc
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Units Unit { get; set; }
+        private double pricePremium;
+        public double PricePremium
+        {
+            get
+            {
+                return pricePremium;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PricePremium argument exception!");
+                pricePremium = value;
+            }
+        }
+        private double stockBalance;
+        public double StockBalance
+        {
+            get
+            {
+                return stockBalance;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("StockBalance argument exception!");
+                stockBalance = value;
+            }
+        }
+        public Provider Provider { get; set; }
+        public Product(string name, Units unit, double pricePremium, double stockBalance, Provider provider)
+        {
+            Name = name;
+            Unit = unit;
+            PricePremium = pricePremium;
+            StockBalance = stockBalance;
+            Provider = provider;
+        }
+    }
+}
---
+++ Sem3_Coursework/Products/Provider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3_Coursework.Products
+{
+    public enum Banks
+    {
+        Sberbank, AlphaBank, TBank, VTB 
+    }
+    public class Provider
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Banks Bank { get; set; }
+        public string Account { get; set; }
+        public string INN { get; set; }
+        
+        public Provider(string name, string address, string fullName, string phoneNumber, Banks bank, string account, string iNN)
+        {
+            Name = name;
+            Address = address;
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Bank = bank;
+            Account = account;
+            INN = iNN;
+        }
+    }
+}
